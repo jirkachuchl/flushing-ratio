ca9b6185d1fcab36e640eedf8e06ab924d1ac800
Jiří Chuchlík <125577695+jirkachuchl@users.noreply.github.com>
2026-02-17 17:09:51 +0100

    Create Bivariate_model.R

---
+++ code/Statistical_Analysis/Bivariate_model.R
@@ -0,0 +1,35 @@
+library(asreml)
+library(asremlPlus)
+source("pin.R")
+
+list.files()
+
+data <- read.csv("DataFull.csv")
+data <- data[order(data$row),]
+data <- data[order(data$col),]
+data <- data[order(data$site),]
+
+# data <- read.csv("Site1.csv")
+# data <- read.csv("Site2.csv")
+
+modelC <- asreml(fixed=cbind(FR_24,FR_25)~1,
+                 random    = ~corgh(trait):clone.new,
+                 residual  = ~id(units):corgh(trait),
+                 data      = data,
+                 na.action = na.method(y="include", x="include"))
+modelC <- update.asreml(modelC)
+summary(modelC)$varcomp
+
+# Type A Genetic correlations
+summary(modelC)$varcomp[1,]
+
+# Zero model
+model0 <- asreml(fixed     = cbind(FR_24,FR_25)~1,
+                 random    = ~diag(trait):clone.new,
+                 residual  = ~id(units):corgh(trait),
+                 data      = data,
+                 na.action = na.method(y="include", x="include"))
+model0 <- update.asreml(model0)
+
+# Likelihood-Ratio Test for statistical significance
+lrt(modelC, model0, boundary = FALSE)
