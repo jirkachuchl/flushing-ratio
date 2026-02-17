fb6fdd0953056374f416ba7c286de2d8b082341f
Jiří Chuchlík <125577695+jirkachuchl@users.noreply.github.com>
2026-02-17 16:53:34 +0100

    Refactor data loading and model calculations
    Updated data loading process and added model calculations for multisite analysis.

--- code/Statistical_Analysis/Multisite_model.R
+++ code/Statistical_Analysis/Multisite_model.R
@@ -4,14 +4,33 @@ source("pin.R")
 
 list.files()
 
-read.csv("DataFull.csv")
-data <- DataFull
+data <- read.csv("DataFull.csv")
 data <- data[order(data$row),]
 data <- data[order(data$col),]
 data <- data[order(data$site),]
 
-load("hermanky.Rdata")
-data<-hermanky
+# data <- read.csv("Site1.csv")
+# data <- read.csv("Site2.csv")
 
-load("lisice.Rdata")
-data<-lisice
+model1 <- asreml(fixed     = VS25 ~ 1 + site,
+                 random    = ~ corh(site):clone.new,
+                 residual  = ~dsum(~ar1(col):ar1(row) | site),
+                 data      = data,
+                 na.action = na.method(y="include", x="include"))
+model1 <- update.asreml(model1)
+summary(model1)$varcomp
+
+# Type B genetic correlation
+summary(model1)$varcomp[1,]
+
+# Site differences 
+wald(model1)
+
+# H2 multisite
+pin(model1,h2_T~(V2+V3)/ (V2+V3+V4+V7))
+
+# H2 Site 1 Hermanky
+pin(model1,h2_T~(V2)/ (V2+V4))
+
+# H2 Site 2 Lisice
+pin(model1,h2_T~(V3)/ (V3+V7))
